29c98f3469353833c1a76541c0db0459f69fe396
FabianSchoenberger <163822878+FabianSchoenberger@users.noreply.github.com>
2025-05-18 11:50:28 +0200

    fix test script
    whoops

--- rover/test.py
+++ rover/test.py
@@ -1,8 +1,10 @@
+import numpy as np
+
 from hardware import *
 
 
 def testServo(servo, low, high):
-    for i in range(90, low, -1):
+    for i in range(0, low, -1):
         set_servo(servo, i)
         time.sleep(0.01)
     for i in range(low, high, 1):
@@ -11,18 +13,25 @@ def testServo(servo, low, high):
     for i in range(high, low, -1):
         set_servo(servo, i)
         time.sleep(0.01)
-    for i in range(low, 90, 1):
+    for i in range(low, 0, 1):
         set_servo(servo, i)
         time.sleep(0.01)
 
 
 def testMotor(motor):
-    for i in range(500, 1000):
+    for i in np.arange(0, 1, 0.01):
         set_motor(motor, i)
         time.sleep(0.001)
-    set_motor(motor, 1000)
-    time.sleep(1)
-    set_motor(motor, 0)
+    for i in np.arange(1, 0, -0.01):
+        set_motor(motor, i)
+        time.sleep(0.001)
+    for i in np.arange(0, -1, -0.01):
+        set_motor(motor, i)
+        time.sleep(0.001)
+    for i in np.arange(-1, 0, 0.01):
+        set_motor(motor, i)
+        time.sleep(0.001)
+
 
 def testLed():
     set_led(LED_RED, 1)
@@ -45,16 +54,17 @@ def testLed():
     set_led(LED_GREEN, 0)
     set_led(LED_BLUE, 0)
 
+
 def testBuzzer():
     set_buzzer(BUZZER, 1)
     time.sleep(1)
     set_buzzer(BUZZER, 0)
 
 
-
-testServo(STEER, 50, 130)
-testServo(CAMERA_X, 70, 150)
-testServo(CAMERA_Y, 40, 140)
+testServo(STEER, -40, 40)
+testServo(CAMERA_X, -20, 60)
+testServo(CAMERA_Y, -50, 50)
 testMotor(LEFT)
 testMotor(RIGHT)
 testLed()
+testBuzzer()
