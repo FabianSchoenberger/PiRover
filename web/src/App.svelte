59ffcb917331cef359cb7b6fdd598f4c5e77824e
Alexander Pichler <alexpichler1904@gmail.com>
2025-05-21 11:50:43 +0200

    added tailwind, added controls for leds and buzzer, make ui a bit nicer

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -36,118 +36,76 @@
             y
         }))
     }
+
+    let led = $state("off");
+    $effect(() => {
+        if (led == "off") handleLed({r: 0, g: 0, b: 0});
+        else if (led == "red") handleLed({r: 1, g: 0, b: 0});
+        else if (led == "orange") handleLed({r: 1, g: 1, b: 0});
+    })
+    function handleLed({r, g, b}: number) {
+        if (!ws) return;
+        console.log("led", r, g, b)
+        ws.send(JSON.stringify({
+            type: "led",
+            red: r,
+            greed: g,
+            blue: b
+        }))
+    }
+
+    let buzzer = $state(false);
+    $effect(() => {handleBuzzer(buzzer ? 1 : 0)})
+    function handleBuzzer(on: number) {
+        if (!ws) return;
+        console.log("buzzer", on)
+        ws.send(JSON.stringify({
+            type: "buzzer",
+            buzzer: on
+        }))
+    }
 </script>
 
 <main>
-    <div class="flex">
-        <img src="../logo.png" class="logo"/>
-        <div class="connect-form">
-            <input type="text" bind:value={ip} class="ip-input" placeholder="Enter IP Address"/>
-            <button onclick={connect} class="connect-button">Connect</button>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 items-center content-center justify-center justify-items-center gap-4">
+        <img src="../logo.png" class="size-40" alt="logo"/>
+        <div class="flex items-center gap-2">
+            <input type="text" bind:value={ip} class="input-primary" placeholder="Enter IP Address"/>
+            <button onclick={connect} class="button-primary">Connect</button>
         </div>
         <Controls {enabled}
                   onDriveChange={handleDriveChange}
                   onSteerChange={handleSteerChange}
                   onCameraChange={handleCameraChange}
         />
-        <div class="slider-container">
-            <label for="speed">Speed Multiplier:</label>
-            <input
-                    type="range"
-                    id="speed"
-                    min="0.1"
-                    max="1"
-                    step="0.01"
-                    bind:value={speedMult}
-            />
-            <span class="value">{speedMult.toFixed(2)}</span>
+        <div class="flex items-center gap-2">
+            <label class="text-primary" for="speed">Speed Multiplier:</label>
+            <input type="range" id="speed" min="0.1" max="1" step="0.01" bind:value={speedMult} class="accent-primary"/>
+            <span class="text-primary">{speedMult.toFixed(2)}</span>
+        </div>
+        <div class="flex items-center gap-2">
+            <button class={led === "red" ? "button-danger" : "button-grey"} onclick={() => {led === "red" ? led = "off" : led = "red"}}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 18v-5.25m0 0a6.01 6.01 0 0 0 1.5-.189m-1.5.189a6.01 6.01 0 0 1-1.5-.189m3.75 7.478a12.06 12.06 0 0 1-4.5 0m3.75 2.383a14.406 14.406 0 0 1-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 1 0-7.517 0c.85.493 1.509 1.333 1.509 2.316V18" />
+                </svg>
+                Red
+            </button>
+            <button class={led === "orange" ? "button-primary" : "button-grey"} onclick={() => {led === "orange" ? led = "off" : led = "orange"}}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 18v-5.25m0 0a6.01 6.01 0 0 0 1.5-.189m-1.5.189a6.01 6.01 0 0 1-1.5-.189m3.75 7.478a12.06 12.06 0 0 1-4.5 0m3.75 2.383a14.406 14.406 0 0 1-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 1 0-7.517 0c.85.493 1.509 1.333 1.509 2.316V18" />
+                </svg>
+                Orange
+            </button>
+            <button class={buzzer === true ? "button-primary" : "button-grey"} onclick={() => {buzzer = !buzzer}}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
+                </svg>
+                Buzzer
+            </button>
         </div>
     </div>
-    <div class="camera">
+    <div class="m-auto">
         <Camera {ws}/>
     </div>
 </main>
 
-<style>
-    :root {
-        --primary-color: #D28F2E;
-        --primary-color-dark: #b37d26;
-    }
-    .camera {
-        margin: auto;
-    }
-    .logo {
-        width: 150px;
-        height: 150px;
-    }
-    .flex {
-        display: flex;
-        justify-content: space-around;
-        align-items: center;
-        margin-bottom: 20px;
-    }
-
-    @media (max-width: 1200px) {
-        .flex {
-            flex-direction: column;
-            gap: 20px;
-            align-items: center;
-        }
-    }
-    .connect-form {
-        display: flex;
-        align-items: center;
-        gap: 10px;
-        font-family: sans-serif;
-    }
-    .ip-input {
-        padding: 10px 12px;
-        border: 1px solid #ccc;
-        border-radius: 4px;
-        font-size: 1em;
-        outline: none;
-        transition: border-color 0.2s ease-in-out, box-shadow 0.2s ease-in-out;
-    }
-    .ip-input:focus {
-        border-color: var(--primary-color);
-        box-shadow: 0 0 0 0.2rem rgba(210, 143, 46, 0.25);
-    }
-    .connect-button {
-        padding: 10px 20px;
-        background-color: var(--primary-color);
-        color: #fff;
-        border: none;
-        border-radius: 4px;
-        font-size: 1em;
-        cursor: pointer;
-        transition: background-color 0.2s ease-in-out;
-    }
-    .connect-button:hover {
-        background-color: var(--primary-color-dark);
-    }
-    .connect-button:active {
-        background-color: var(--primary-color-dark);
-    }
-    .connect-button:focus {
-        outline: none;
-        box-shadow: 0 0 0 0.2rem rgba(210, 143, 46, 0.25);
-    }
-    .slider-container {
-        display: flex;
-        align-items: center;
-        gap: 10px;
-        font-family: sans-serif;
-    }
-
-    input[type="range"] {
-        width: 200px;
-        accent-color: var(--primary-color);
-    }
-
-    .value {
-        font-size: 1em;
-        color: var(--primary-color);
-        width: 40px;
-        text-align: right;
-    }
-</style>
--- web/src/lib/Controls.svelte
+++ web/src/lib/Controls.svelte
@@ -36,78 +36,19 @@
         source.reset();
         source = gamepad;
     }
-
-    function isKeyboardSelected(): boolean {
-        return source === keyboard;
-    }
-
-    function isGamepadSelected(): boolean {
-        return source === gamepad;
-    }
 </script>
 
-<div class="input-method-selector">
-    <button
-            class="method-button"
-            on:click={setKeyboard}
-            class:selected={isKeyboardSelected()}
-            class:unselected={!isKeyboardSelected()}
-    >
+<div class="flex gap-4">
+    <button class="{source === keyboard ? 'button-primary' : 'button-grey'}" on:click={setKeyboard}>
         <span class="icon keyboard-icon">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-keyboard"><path d="M10 8h.01"/><path d="M12 12h.01"/><path d="M14 8h.01"/><path d="M16 12h.01"/><path d="M18 8h.01"/><path d="M6 8h.01"/><path d="M7 16h10"/><path d="M8 12h.01"/><rect width="20" height="16" x="2" y="4" rx="2"/></svg>
         </span>
         Keyboard
     </button>
-    <button
-            class="method-button"
-            on:click={setGamepad}
-            class:selected={isGamepadSelected()}
-            class:unselected={!isGamepadSelected()}
-    >
+    <button class="{source === gamepad ? 'button-primary' : 'button-grey'}" on:click={setGamepad}>
         <span class="icon gamepad-icon">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-gamepad2-icon lucide-gamepad-2"><line x1="6" x2="10" y1="11" y2="11"/><line x1="8" x2="8" y1="9" y2="13"/><line x1="15" x2="15.01" y1="12" y2="12"/><line x1="18" x2="18.01" y1="10"/><path d="M17.32 5H6.68a4 4 0 0 0-3.978 3.59c-.006.052-.01.101-.017.152C2.604 9.416 2 14.456 2 16a3 3 0 0 0 3 3c1 0 1.5-.5 2-1l1.414-1.414A2 2 0 0 1 9.828 16h4.344a2 2 0 0 1 1.414.586L17 18c.5.5 1 1 2 1a3 3 0 0 0 3-3c0-1.545-.604-6.584-.685-7.258-.007-.05-.011-.1-.017-.151A4 4 0 0 0 17.32 5z"/></svg>
         </span>
         Controller
     </button>
 </div>
-
-<style>
-    :root {
-        --primary-color: #D28F2E;
-        --primary-color-dark: #b37d26;
-        --text-color-light: #f0f0f0;
-        --text-color-dark: #333;
-        --disabled-color: #777;
-    }
-
-    .input-method-selector {
-        display: flex;
-        gap: 10px;
-    }
-
-    .method-button {
-        padding: 10px 20px;
-        border: none;
-        border-radius: 4px;
-        font-size: 1em;
-        cursor: pointer;
-        transition: background-color 0.2s ease-in-out;
-        display: flex;
-        align-items: center;
-        gap: 10px;
-    }
-
-    .method-button.selected {
-        background-color: var(--primary-color);
-        color: var(--text-color-light);
-    }
-
-    .method-button.selected:hover {
-        background-color: var(--primary-color-dark);
-    }
-
-    .method-button.unselected {
-        background-color: #eee;
-        color: var(--disabled-color);
-    }
-</style>
