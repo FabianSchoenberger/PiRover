5ed3265a73a0c0e154d3daae6804156d5c3405fb
Philipp Olivotto <58550171+zbaakez@users.noreply.github.com>
2025-05-22 21:49:16 +0200

    Grafana + Postgres Implementation
    Grafana Implementation with Docker
    Works based on PostgresDB with the TimescaleDB extension.
    Python scripts for saving MQTT published data on rover/# to Postgres, and a simple data simulation script.

---
+++ monitoring/initdb/01-setup-grafanadb.sql
@@ -0,0 +1,25 @@
+CREATE USER "grafana_user" WITH PASSWORD 'grafana_password_placeholder'; -- grafana_password_placeholder
+CREATE DATABASE grafana_db;
+
+\connect grafana_db
+
+CREATE EXTENSION IF NOT EXISTS timescaledb;
+
+-- One table for all rover readings, sensors will be differentiated by the sensor_topic column
+CREATE TABLE IF NOT EXISTS rover_readings (
+    id SERIAL NOT NULL,
+    time TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    sensor_topic TEXT NOT NULL,
+    value DOUBLE PRECISION NOT NULL,
+    PRIMARY KEY (id, time)
+);
+
+-- Convert the table to a TimescaleDB hypertable
+SELECT create_hypertable('rover_readings', 'time');
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON public.rover_readings TO grafana_user;
+
+GRANT USAGE ON SCHEMA public TO grafana_user;
+GRANT CREATE ON SCHEMA public TO grafana_user;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO grafana_user;
+GRANT USAGE, SELECT ON SEQUENCE rover_readings_id_seq TO grafana_user;
